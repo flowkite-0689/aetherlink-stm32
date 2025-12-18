94c36e41e2d069390a04a35bb556f24f138d42e2
kes <2604338097@qq.com>
2025-12-18 11:01:04 +0800

    feat(esp8266): 将函数参数类型修改为 const char* 以增强安全性
    将 ESP8266 相关函数的字符串参数改为 const char* 类型，防止意外修改字符串内容，提高代码健壮性。同时，将 ESP8266 主任务从 main.c 移植到 esp8266.c 文件中，并完善了 WiFi、服务器连接及订阅主题的重试机制，增强了网络连接的稳定性。此外，新增对 RTC 时间同步的支持，并优化了传感器数据发布逻辑。

--- User/WIFI/esp8266.h
+++ User/WIFI/esp8266.h
@@ -9,6 +9,9 @@
 #include "stm32f10x.h"
 #include <stdint.h>
 #include "sensordata.h"
+#include "rtc_date.h"
+#include "oled_print.h"
+
 extern uint8_t uart2_buffer[UART2_BUF_SIZE]; // uart2接收缓冲
 extern uint8_t uart2_rx_len;     // uart2接收长度
 
@@ -17,14 +20,17 @@ extern uint8_t Server_connected;
 extern uint16_t publish_delaytime;
 
 void ESP8266_Receive_Start(void);
-uint8_t ESP8266_Connect_WiFi(char *ssid, char *password);
-uint8_t ESP8266_Connect_Server(char *ip, char *port);
-uint8_t ESP8266_TCP_Subscribe(char *uid, char *topic);
-uint8_t ESP8266_TCP_Publish(char *uid, char *topic, char *data);
+uint8_t ESP8266_Connect_WiFi(const char *ssid,const char *password);
+uint8_t ESP8266_Connect_Server(const char *ip,const char *port);
+uint8_t ESP8266_TCP_Subscribe(const char *uid,const char *topic);
+uint8_t ESP8266_TCP_Publish(const char *uid,const char *topic, char *data);
 uint8_t ESP8266_TCP_Heartbeat(void);
-uint8_t ESP8266_TCP_GetTime(char *uid, char *time_buffer, uint16_t buffer_size);
+uint8_t ESP8266_TCP_GetTime(const char *uid, char *time_buffer, uint16_t buffer_size);
 
 // 新增消息解析函数
 uint8_t ESP8266_Parse_Command(const char *buffer, const char *topic, char *msg_value);
 uint8_t ESP8266_Process_Sensor_Commands(const char *buffer);
+
+
+static void ESP8266_Main_Task(void *pvParameters);
 #endif 
